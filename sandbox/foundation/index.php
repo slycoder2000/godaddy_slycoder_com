be5296128f484337af77575ccbc6d13b7bf96cc8
slycoder2000 <sly@slycoder.com>
2019-01-29 01:42:33 -0700

    Utilities
    Initial Commit

---
+++ sandbox/foundation/index.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Foundation | Foundation</title>
+
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/motion-ui/1.2.3/motion-ui.css">
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+
+		<div class="grid-y" id="container">
+			<div class="large-12 medium-12 cell">
+
+				<!-- Nav -->
+
+				<div class="top-bar" height="20px;">
+					<div class="top-bar-title">
+						<span data-responsive-toggle="responsive-menu" data-hide-for="medium">
+							<button class="menu-icon dark" type="button" data-toggle></button>
+						</span>
+						<a class="navbar-brand" href="index.php">
+						<img src="images/slycoder_icon.png" alt="Slycoder.com" style="height:40px">
+						</a>
+					</div>
+					<div id="responsive-menu" data-animate="hinge-in-from-top spin-out">
+						<div class="top-bar-left">
+							<ul class="dropdown menu" data-dropdown-menu>
+								<li><a href="index.php">Home</a></li>
+								<li><a href="about.php">About</a></li>
+								<li><a href="contact.php">Contact</a></li>
+
+								<li><a href="resume.php">Resume</a>
+									<ul class="menu vertical">
+										<li><a href="resume.php">Professional Profile</a></li>
+										<li><a href="resume.php#skills">Skills</a></li>
+										<li><a href="resume.php#workhistory">Work History</a></li>
+										<li><a href="resume.php#otherworkhistory">Other Work History</a></li>
+										<li><a href="resume.php#education">Education &amp; Credentials</a></li>
+										<li><a href="resume.php#download">Download Resume</a></li>
+									</ul>
+								</li>
+								<li><a href="clients.php">Clients</a></li>			
+								<li><a href="resources.php">Resources</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>		
+
+
+				<!-- End of Nav -->			
+				
+				<!-- Main Content -->
+
+				<div class="grid-y maincontent">
+					<div class="large-12 cell">
+						<div class="primary callout">
+							<h1>About Me</h1>
+						</div>
+
+						<div class="primary callout">
+							<p>
+							I work with a number of people to help promote their businesses and personal sites with 24+ years experience in computer programming, web development and pc repair.
+							</p>
+							<p>
+							I communicate in a non-technical manner in laymen terms. Unless you actually want the down and dirty tech-savvy lingo
+							</p>  
+						</div>
+
+						<!-- FOOTER -->
+						<footer>
+							<a class="button float-right" href="#" role="button">Back to top</a>
+                        </footer>
+
+					</div>
+				</div>
+
+			</div>
+		</div>
+				
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/what-input/5.0.5/what-input.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/foundation.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/motion-ui/1.2.3/motion-ui.js"></script>
+		<script src="http://www.slycoder.com/scripts/foundation/js/app.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.js"></script>
+		<script type="text/javascript" src="frag_footer.js"></script>
+
+	</body>
+</html>
